e6f2f0aff8ff711861d75c3e75958600b04f1736
Griffith Baker <gmbaker3@ncsu.edu>
2020-08-11 17:18:20 -0400

    Updated to work with latest site version
    Also added directory saving feature.

--- WebAssignHW/WebAssignHW.py
+++ WebAssignHW/WebAssignHW.py
@@ -1,48 +1,61 @@
-from selenium						import webdriver
-from selenium.webdriver.common.keys	import Keys
-from selenium.webdriver.support.ui	import Select
-from dateutil.parser				import parse
-from dateutil.tz					import gettz
-from datetime						import timedelta, datetime
-from ics							import Calendar, Event
+from selenium	import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from dateutil.parser import parse
+from dateutil.tz import gettz
+from datetime import timedelta, datetime
+from ics import Calendar, Event
 import time
+import os
 
-#Given a Webassign course page return homework 
+#Given a Webassign course page return homework
 #Return list in format {'assignment':'datetime '}
 def get_hw(selenium, course):
-	name = selenium.find_elements_by_xpath("//span[@class='rowLeft']")
-	due = selenium.find_elements_by_xpath("//span[@class='rowRightNarrow']")
-	hw = dict(zip([option.text for option in name], [option.text for option in due]))
-	hw.pop('Name')
+	name = selenium.find_elements_by_xpath("//*[@id='js-student-myAssignmentsWrapper']/section/ul/li/a/div[1]/span")
+	print("**NAME**")
+	print(name[0].text)
+	due = selenium.find_elements_by_xpath("//*[@id='js-student-myAssignmentsWrapper']/section/ul/li/a/div[3]")
+	link = selenium.find_elements_by_xpath("//*[@id='js-student-myAssignmentsWrapper']/section/ul/li/a")
+	hw = list(zip([option.text for option in name], [option.text for option in due], [option.get_attribute('href') for option in link]))
 	return hw
 
 #Returns compiled list of homework of all classes
-def all_homework(url, user, pwd):
-	#Sets up browser
+def all_homework(auth_url, homepage_url, user, pwd):
 	try:
-		selenium = webdriver.PhantomJS()
-		selenium.get(url)
+		options = webdriver.ChromeOptions()
+		options.add_argument('window-size=1200x600')
+		options.add_argument('headless')
+		selenium = webdriver.Chrome(chrome_options=options)
+		selenium.get(auth_url)
 		selenium.implicitly_wait(5)
 
 		#Sends username and pass
 		userfield=selenium.find_element_by_id("email").send_keys(user)
 		pwdfield= selenium.find_element_by_id("cengagePassword").send_keys(pwd)
+		time.sleep(3)
+		selenium.get(homepage_url)
 	except:
+		
 		print('Could Not Connect')
 		exit()
 
 	#Counter index for list of classes as first two options aren't courses
-	class_num = 2
+	class_num = 1
 
 	homework={}
 
-	#Loops thru dropdown menu 
+	#Loops thru dropdown menu
 	while(True):
-			select = selenium.find_element_by_id('course') #Gathers array of courses
-			options = select.find_elements_by_tag_name("option") #Gets lenght for break 
+			select = selenium.find_element_by_id('courseSelect') #Gathers array of courses
+			options = select.find_elements_by_tag_name("option") #Gets lenght for break
+			print("**OPTIONS**")
+			print(options[class_num].text)
 			course=options[class_num].text
-			if len(options)>2: options[class_num].click()
-			else: break
+			if len(options)>=1: 
+				options[class_num].click()
+				selenium.find_element_by_css_selector('#js-page-top > div > div > div:nth-child(1) > nav > div > button').click()
+			else: 
+				break
 			homework[course]=get_hw(selenium,course) #stores assingments in form of {'class':{'hw','due'}}
 			class_num += 1
 			selenium.back()
@@ -56,23 +69,29 @@ start_time = datetime.now()
 #Read user and pass from file
 file = open('account.key','r')
 key = file.readlines()
-url		= key[0].replace('url=','')
-user	= key[1].replace('user=','')
-pwd		= key[2].replace('pass=','')
-tz		= key[3].replace('timezone=','')
+auth_url     = key[0].replace('auth_url=','')
+homepage_url = key[1].replace('homepage_url=','')
+user	     = key[2].replace('user=','')
+pwd	     = key[3].replace('pass=','')
+tz	     = key[4].replace('timezone=','')
+directory_to_write = key[5].replace('directory=','')
 file.close()
 
-homework = all_homework(url,user,pwd) #Grabs all homework onto list
+homework = all_homework(auth_url, homepage_url, user, pwd) #Grabs all homework onto list
 
 cal = Calendar()
 
 for course, assignment in homework.items():
-	for homework, due in assignment.items():
+	print(course)
+	print(assignment)
+	for homework, due, link in assignment:
 		time = parse(due).astimezone(gettz(tz))
-		print('{} {} {}'.format(homework, due, time))
-		cal.events.append(Event(name=homework,begin=time-timedelta(hours=1),end=time,description=course))
+		print('{} {} {} {}'.format(homework, due, time, link))
+		e = Event(name=homework, begin=time-timedelta(hours=1), end=time, duration=None, description=course, created=None, last_modified=None, location=link, url=None, transparent=None, alarms=None, attendees=None, categories=None, status=None, organizer=None, geo=None, classification=None)
+		cal.events.add(e)
 
-with open('WebAssign.ics', 'w') as f:
+
+with open(os.path.join(directory_to_write, 'WebAssign.ics'), "w") as f:
     f.writelines(cal)
 
 end_time = datetime.now() - start_time
